0b0a46243e622d804781bf041620663f96c66351
zhila aghajari <zhila.aghajari88@gmail.com>
2020-02-06 09:06:53 -0500

    up

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import threading
 import math
 import random
 from random import randint
-
+import urllib.request
 # Create the hash-table of this server ..
 number_keys = 5000
 node_numbers = 3
@@ -64,8 +64,8 @@ server_ip_mac_mini = '128.180.220.113' #university
 #server_ip_mac_mini = '192.168.1.8' #at home
 server_ip_mac_book = '128.180.204.171'#university
 #server_ip_mac_book = '192.168.1.5'
-#my_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')#get my public id
-my_ip = '192.168.1.8'
+my_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')#get my public id
+#my_ip = '192.168.1.8' #home
 if my_ip == server_ip_mac_mini:
     node_id = 0
 elif my_ip == server_ip_mac_book:
