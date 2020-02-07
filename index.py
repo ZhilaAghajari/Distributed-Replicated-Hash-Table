f61d7c3baea2e5fd1b7856b6663894a8eb5ed1b3
zhila aghajari <zhila.aghajari88@gmail.com>
2020-02-07 10:48:08 -0500

    o

--- index.py
+++ index.py
@@ -129,8 +129,8 @@ while True:
         #send the data back to the application who requested this request?
         print('sending result of the request back to the client side')
         print('@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-        # if res==None:
-        #     res = "None"
+        if res==None:
+            res = "None"
         # else:
         #     res = "Success"
         connection.sendall(str(res).encode())
